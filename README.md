0208b30decc3d92f4c0cd03b5dd74fc88ced6160
hsahu11 <himanshu.1689@gmail.com>
2020-08-15 11:08:56 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+/* Himanshu Sahu 15/08/20  */
+Cousera assignment submission for course embedded system.
+Program to do the data analytics on array 
---
+++ stats.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <Add File Name> 
+ * @brief <Add Brief Description Here >
+ *
+ * <Add Extended Description Here>
+ *
+ * @author <Himanshu Sahu>
+ * @date <15/08/2020>
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+void print_statistics(int a[]);
+void print_array(int a[]);
+int find_median(int a[]);
+float find_mean()(int a[]);
+int find_maximum() (int a[]);
+int find_minimum()(int a[]);
+void sort_array()(int a[]);
+
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
